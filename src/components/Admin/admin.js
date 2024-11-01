504d62e89279e89ac823401cd426580d8a03033c
AnhDucPhan <phanducanh21032003@gmail.com>
2024-11-02 01:53:06 +0700

    update component admin:sidebar

--- src/components/Admin/admin.js
+++ src/components/Admin/admin.js
@@ -1,7 +1,24 @@
+import SideBar from "./sidebar"
+import './admin.scss'
+import { FaBars } from 'react-icons/fa'
+import { useState } from "react"
+
 const Admin = (props) => {
+    const [collapsed, setCollapsed] = useState(false)
+    // const handleToggle = () => {
+    //     setCollapsed(!collapsed)
+    // }
     return (
         <div>
-            This is component Admin
+            <div className="admin-container">
+                <div className="admin-sidebar">
+                    <SideBar collapsed={collapsed} />
+                </div>
+                <div className="admin-content">
+                    <FaBars onClick={() => { setCollapsed(!collapsed) }}></FaBars>
+                    This is component Admin
+                </div>
+            </div>
         </div>
     )
 }
